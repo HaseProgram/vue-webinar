c41667c83bfa59564e239ff184ae90eb15611957
Dmitry Zaytsev <hello@dzaytsev.pro>
2020-03-10 18:09:44 +0300

    added template in button

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="[$style.wrapper]">Моя кнопка</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style module lang="scss">
+@import "../styles/base.scss";
+
+.wrapper {
+  display: inline-block;
+  padding: 8px 16px;
+  background: $dark-green;
+  color: rgb(249, 249, 249);
+  border-radius: 4px;
+  cursor: pointer;
+  user-select: none;
+
+  &:hover {
+    background: $dark-green-hover;
+  }
+}
+</style>
